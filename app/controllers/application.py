2a87bdb25bef784a9e9d29964680395aee6de365
LuizHPio <luizxgold@gmail.com>
2025-11-13 20:27:20 -0300

    Add routing for home, login, and register pages; update landing route

--- app/controllers/application.py
+++ app/controllers/application.py
@@ -5,6 +5,10 @@ class Application():
 
     def __init__(self):
         self.pages = {
+            "landing": self.landing,
+            "login": self.login,
+            "register": self.register,
+            "home": self.home
         }
 
     def render(self, page):
@@ -13,3 +17,15 @@ class Application():
 
     def helper(self):
         return template('app/views/html/helper')
+
+    def landing(self):
+        return template('app/views/html/landing')
+
+    def login(self):
+        return template('app/views/html/login')
+
+    def register(self):
+        return template('app/views/html/register')
+
+    def home(self):
+        return template('app/views/html/home', salas=[], equipamentos=[])
--- route.py
+++ route.py
@@ -1,4 +1,5 @@
 from app.controllers.application import Application
+from app.controllers.registration import Registration
 from bottle import Bottle, route, run, request, static_file
 from bottle import redirect, template, response
 
@@ -15,16 +16,41 @@ def serve_static(filepath):
     return static_file(filepath, root='./app/static')
 
 
-@app.route('/')
+@app.route('/helper')
 def helper(info=None):
     return ctl.render('helper')
 
 
 # -----------------------------------------------------------------------------
 # Suas rotas aqui:
+@app.route('/home')
+def helper(info=None):
+    return ctl.render('home')
+
+
+@app.route('/register', method=['GET', 'POST'])
+def helper(info=None):
+    if request.method == "POST":
+        email = request.forms.get("email")
+        password = request.forms.get("senha")
+        name = request.forms.get("nome")
+        return Registration.register_user(response, name, email, password)
+
+    return ctl.render('register')
+
+
+@app.route('/login')
+def helper(info=None):
+    return ctl.render('login')
+
+
+@app.route('/')
+def helper(info=None):
+    return ctl.render('landing')
 
 
 # -----------------------------------------------------------------------------
+
 
 if __name__ == '__main__':
 
